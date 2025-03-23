26722db7847c36fc799fd2b068ecac7390bc4696
Black foxi <dentef767@gmail.com>
2025-03-23 09:34:04 +0000

    change card

--- components/skeletons/userCardSkeleton.tsx
+++ components/skeletons/userCardSkeleton.tsx
@@ -5,13 +5,12 @@ import { Skeleton } from '@/components/ui/skeleton'
 const UserCardSkeleton = () => {
   return (
     <div className="block">
-      <Card className="bg-white transition-all duration-300 ease-in-out">
+      <Card className="text-white transition-all duration-300 ease-in-out">
         <CardHeader>
-          <Skeleton className="h-6 w-3/4 bg-violet-950" />
+          <Skeleton className="h-6 w-3/4 bg-gray-300" />
         </CardHeader>
         <CardContent className="space-y-2">
-          <Skeleton className="h-4 w-2/3 bg-violet-950" />
-          <Skeleton className="h-4 w-3/4 bg-violet-950" />
+          <Skeleton className="h-4 w-2/3 bg-gray-300" />
         </CardContent>
       </Card>
     </div>
--- components/user/userCard.tsx
+++ components/user/userCard.tsx
@@ -8,8 +8,8 @@ const UserCard = (
     { user }: { user: User }
 ) => {
     return (
-        <Link href={`/users/${user.id}`} className='group block' >
-            <Card className='bg-white transition-all duration-300 ease-in-out transform group-hover:scale-105 group-hover:shadow-lg group-hover:shadow-white group-hover:brightness-75'>
+        <Link href={`/users/${user.id}`} className='group block text-white' >
+            <Card className=' transition-all duration-300 ease-in-out transform group-hover:scale-105 group-hover:shadow-lg group-hover:shadow-white group-hover:brightness-75'>
                 <CardHeader>
                     <CardTitle>{user.name}</CardTitle>
                 </CardHeader>
